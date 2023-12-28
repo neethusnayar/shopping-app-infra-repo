cd731004000f2536b5d66767c77eddcc2615aa6c
neethu <neethusnayar@gmail.com>
2023-12-28 11:46:07 +0000

    prometheus-port

--- main.tf
+++ main.tf
@@ -34,7 +34,13 @@ resource "aws_security_group" "http_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+  ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   ingress {
     from_port        = 443
