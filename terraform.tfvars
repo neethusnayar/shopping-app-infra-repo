740ef6c4d89b80489eb478bcfe3572d69697b328
neethu <neethusnayar@gmail.com>
2023-12-29 11:37:47 +0000

    initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+project_name  = "shopping"
+project_env  = "prod"
+hosted_zone_name = "thirdeyestechnicalservices.com"
+hosted_zone_id = "Z0978374ZFSQ1C8AJJM9"
+hostname = "shopping"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {}
+variable "instance_ami" {}
+variable "project_name" {}
+variable "project_env" {}
+variable "hosted_zone_name" {} 
+variable "hosted_zone_id" {}
+variable "hostname" {}
+
